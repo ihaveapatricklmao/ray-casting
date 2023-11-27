004e5a0bf16b03ff685af8fdee39212cc43ba618
ihaveapatricklmao <82519853+ihaveapatricklmao@users.noreply.github.com>
2023-11-27 18:31:14 -0500

    getting started in general logic and other stuff

--- src/logic.h
+++ src/logic.h
@@ -2,8 +2,32 @@
 #ifndef LOGIC_H
 #define LOGIC_H
 
+
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include "include/glm/glm.hpp"
 #include "include/camera.h"
 #include "include/entities.h"
 #include "include/walls.h"
+
+
+namespace World {
+
+	struct points {
+		glm::vec2 point_pos;
+	};
+
+	class grid {
+		points pt_a;
+		points pt_b;
+		points pt_c;
+		points pt_d;
+	};
+
+	struct space {
+		std::vector<grid> _grids;
+	};
+}
 
 #endif // !LOGIC_H
