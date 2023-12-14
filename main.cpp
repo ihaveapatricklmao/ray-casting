75b64c939fddc506f3eb6395b227371042fa78af
ihaveapatricklmao <82519853+ihaveapatricklmao@users.noreply.github.com>
2023-12-14 09:56:50 -0500

    window events and tweaks

--- main.cpp
+++ main.cpp
@@ -12,5 +12,8 @@ int main() {
 	Renderer new_render(new_window, 600, 600);
 	new_render.createWindow();
 
+	// stuff can be done under here for when the game is closed
+	std::cout << "quitted!";
+
 	return -1;
 }
--- src/render.hpp
+++ src/render.hpp
@@ -16,7 +16,6 @@ namespace Render {
 	struct Pixel {
 		vec2 pos;
 		vec4 rgba;
-		unsigned int width;
 	};
 
 	struct Caster {
@@ -30,25 +29,24 @@ namespace Render {
 			int w = _surface->w;
 			int h = _surface->h;
 
-			Pixel pix;
+
+			Pixel* pix = new Pixel;
 
 			SDL_LockSurface(_surface);
 			Uint32* pixels = (Uint32*)_surface->pixels;
 			Uint8 r, g, b, a;
 
 			for (int y = 0; y < h; y++) {
-				for (int x = 0; x < w; x += 4) {
-					for (int i = 0; i < 4; i++) {
-						pix.pos = { x + i, y };
-						//std::cout << pix.pos[0] << " , " << pix.pos[1] << "\n";
-						SDL_GetRGBA(pixels[x + i + y * w], _surface->format, &r, &g, &b, &a);
-						pix.rgba = { r, g, b, a };
-						pixels[x + i + y * w] = SDL_MapRGBA(_surface->format, 200, 200, 15, 255);
-						//std::cout << pix.rgba[0] << " , " << pix.rgba[1] << " , " << pix.rgba[2] << " , " << pix.rgba[3] << "\n";
-						_pixels.emplace_back(pix);
-					}
+				for (int x = 0; x < w; x++) {
+					pixels[x + y * w] = SDL_MapRGBA(_surface->format, 0, 170, 95, 255);
+					pix->pos = { x, y };
+					SDL_GetRGBA(pixels[x + y * w], _surface->format, &r, &g, &b, &a);
+					pix->rgba = { r, g, b, a };
+					_pixels.emplace_back(*pix);
 				}
 			}
+
+			delete pix;
 
 			SDL_UnlockSurface(_surface);
 		}
@@ -77,6 +75,8 @@ namespace Render {
 		}
 
 		int createWindow() {
+			SDL_SetMainReady();
+
 			int is_sdl_init = initSdl();
 
 			if (is_sdl_init == 0) {
@@ -84,19 +84,24 @@ namespace Render {
 				window = SDL_CreateWindow("TEST", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, w, h, SDL_WINDOW_RESIZABLE);
 				SDL_Surface* surface = SDL_GetWindowSurface(window);
 				raycast._surface = surface;
-				SDL_Event window_events;
-				window_events.type = SDL_WINDOWEVENT;
+				SDL_Event* window_events = new SDL_Event;
+				window_events->type = SDL_WINDOWEVENT;
 
 				SDL_Renderer* render = SDL_CreateRenderer(window, -1, 0);
 
-				while (window_events.type != SDL_QUIT) {
+				while (window_events->type != SDL_QUIT) {
 
-					if (SDL_PollEvent(&window_events)) {
+					if (SDL_PollEvent(window_events)) {
 
-						switch (window_events.type) {
+						switch (window_events->window.event) {
 							case SDL_WINDOWEVENT_RESIZED:
-								SDL_SetWindowSize(window, window_events.window.data1, window_events.window.data2);
-								SDL_GetWindowSize(window, &window_events.window.data1, &window_events.window.data2);
+								SDL_GetWindowSize(window, &window_events->window.data1, &window_events->window.data2);
+								SDL_SetWindowSize(window, window_events->window.data1, window_events->window.data2);
+								SDL_Log("Window %d resized to %dx%d",
+									window_events->window.windowID, window_events->window.data1,
+									window_events->window.data2);
+
+								raycast.assignRgba();
 								break;
 							case SDL_QUIT:
 								SDL_DestroyRenderer(render);
